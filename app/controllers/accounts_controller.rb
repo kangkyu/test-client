44cde0af375153c52324822385435b96960583d1
Kang-Kyu Lee <kangkyu1111@gmail.com>
2025-02-25 18:10:07 -0800

    Fix typo

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -8,13 +8,13 @@ class AccountsController < ApplicationController
   private
 
   def set_account
-    lsp_accout = Graphql.current_account
+    lsp_account = Graphql.current_account
     unless Account.exists?(lightspark_id: lsp_account.account_id)
       Account.create!(
         lightspark_id: lsp_account.account_id,
-        account_name: lsp_accout.account_name,
-        account_created_at: lsp_accout.account_created_at,
-        account_updated_at: lsp_accout.account_updated_at
+        account_name: lsp_account.account_name,
+        account_created_at: lsp_account.account_created_at,
+        account_updated_at: lsp_account.account_updated_at
       )
     end
     @account = Account.find_by lightspark_id: lsp_account.account_id
