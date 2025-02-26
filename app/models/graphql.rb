813814cc399e4c41959f20f6ef0ba99631288da1
Kang-Kyu Lee <kangkyu1111@gmail.com>
2025-02-25 16:49:34 -0800

    Remove comments; do not use graphql-client

--- app/models/graphql.rb
+++ app/models/graphql.rb
@@ -1,24 +1,3 @@
-# require "graphql/client"
-# require "graphql/client/http"
-
-# # Lightspark API wrapper
-# module Lightspark
-#   # Configure GraphQL endpoint using the basic HTTP network adapter.
-#   HTTP = GraphQL::Client::HTTP.new("https://api.lightspark.com/graphql/server/2023-09-13")
-
-#   # Fetch latest schema on init, this will make a network request
-#   # Schema = GraphQL::Client.load_schema(HTTP)
-
-#   # However, it's smart to dump this to a JSON file and load from disk
-#   #
-#   # Run it from a script or rake task
-#   #   GraphQL::Client.dump_schema(Lightspark::HTTP, "path/to/schema.json")
-#   #
-#   Schema = GraphQL::Client.load_schema("schema.json")
-
-#   Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
-# end
-
 require 'net/http'
 
 module Graphql
